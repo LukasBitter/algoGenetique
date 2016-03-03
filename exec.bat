0ed2266a290990e32179dfe0d7051a19a802223f
lukas <lukas.bitter@gmail.com>
2016-03-03 19:08:08 +0100

    exe for test

---
+++ exec.bat
@@ -0,0 +1,6 @@
+python BitterRyter.py "data/pb050.txt" True 30
+python BitterRyter.py "data/pb050.txt" True 30
+python BitterRyter.py "data/pb050.txt" True 30
+python BitterRyter.py "data/pb050.txt" True 30
+python BitterRyter.py "data/pb050.txt" True 30
+pause 
